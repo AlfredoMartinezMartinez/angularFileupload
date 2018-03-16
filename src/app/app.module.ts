a61fb3f6cc928114e4482f48367b022bfdc68eaf
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-16 11:23:00 +0100

    Creacion de componentes y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//rutas
+import { APP_ROUTES } from './app.routes';
+
 
 
 import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
