c417a97584c3586c0ca4d511ffe56642dc3807b1
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-16 18:05:13 +0100

    validaciones en directiva

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,9 +1,12 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
+
+  @Input() archivos: FileItem[] = [];
 
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
@@ -14,9 +17,44 @@ export class NgDropFilesDirective {
     this.mouseSobre.emit(true);
   }
 
-  // @HostListener('dragleave', ['$event'] )
-  // public onDragLeave(event: any ) {
-  //   this.mouseSobre.emit(false);
-  // }
+  @HostListener('dragleave', ['$event'] )
+  public onDragLeave(event: any ) {
+    this.mouseSobre.emit(false);
+  }
+
+  // validaciones
+  // combinacion de archivo dropeado y es imagen
+  private _archivoPuedeSerCargado(archivo: File): boolean {
+    if ( !this._archivoYaFueDropeado(archivo.name) && this._esImagen(archivo.type)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // al soltar archivo prevenir que se abra en venta del navegador
+  private _prevenirDetener( event ) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  // comprobar que archivo no exista en el array de archivos
+  private _archivoYaFueDropeado( nombreArchivo: string): boolean  {
+    for (const archivo of this.archivos) {
+      if (archivo.nombreArchivo === nombreArchivo) {
+        console.log('el archivo' + nombreArchivo + 'ya esta agregado');
+        return true;
+      }
+
+    }
+    return false;
+  }
+
+  // verificar que sean imagenes
+
+  private _esImagen(tipoArchivo: string ): boolean {
+
+    return ( tipoArchivo === '' || tipoArchivo === undefined ) ? false : tipoArchivo.startsWith('image');
+  }
 
 }
