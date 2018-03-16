fcd7d8f6585508b1581f2071a58e4f7f6337e0d0
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-16 12:29:49 +0100

    Modulos de firebase,Servicio y agregar configuracion Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 // rutas
 import { APP_ROUTES } from './app.routes';
 
-
+// servicio
+import { CargaImagenesService } from './providers/carga-imagenes.service';
 
 import { AppComponent } from './app.component';
 import { FotosComponent } from './components/fotos/fotos.component';
 import { CargaComponent } from './components/carga/carga.component';
+
+// Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from '../environments/environment';
+
 
 
 @NgModule({
@@ -18,9 +28,14 @@ import { CargaComponent } from './components/carga/carga.component';
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
+
   ],
-  providers: [],
+  providers: [CargaImagenesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDlx52phZIGDU-I3FiXU7qDBHRXnh_3Eas',
+    authDomain: 'fir-fotos-406c2.firebaseapp.com',
+    databaseURL: 'https://fir-fotos-406c2.firebaseio.com',
+    projectId: 'fir-fotos-406c2',
+    storageBucket: 'fir-fotos-406c2.appspot.com',
+    messagingSenderId: '909852816580'
+  }
 };
